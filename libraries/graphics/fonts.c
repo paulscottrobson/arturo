303399a11a9e9720ac07734bcff3fd031bd81d46
Paul Robson <paul@robsons.org.uk>
2025-01-03 18:05:45 +0000

    Working Adafruit fonts, no bitmaps

--- libraries/graphics/fonts.c
+++ libraries/graphics/fonts.c
@@ -30,49 +30,39 @@
 //
 // ***************************************************************************************
 
-static int _DrawCharacter(GFXPort *vp,int xPos,int yPos,int ch,int fontid,int colour,int scale) {
+static int _DrawCharacter(GFXPort *vp,int xPos,int yPos,int ch,int fontid,int colour,int scale) {	
 
-    // if (fontid < 0 || fontid >= FONT_COUNT) return 0;                               // Unknown font
-    // const uint8_t *font = fontTable[fontid];
-    // if (ch < font[0] || ch > font[1]) return 0;                                     // Unknown character.
-    // int offset = 64 + (ch - font[0]) * 2;                                           // Address of the offset
-    // offset = font[offset] + (font[offset+1] << 8);                                  // Convert to the actual offset.
-    // if (offset == 0xFFFF) return 0;                                                 // Font not defined.
-    // const uint8_t *chData = font + offset;                                          // Address of the font character record
-    // const uint8_t *bitmapData = chData+3;                                           // Binary data from here.
-    // uint8_t bitMask = 0x80;                                                         // Bit to check.
+	if (fontid < 0 || fontid >= FONT_COUNT) return 0;                               // Unknown font
+	const FONTInfo *font = _dictionary[fontid];   									// Get the font data	
+	if (ch < font->first || ch > font->last) return 0;                   	        // Unknown character.
+	FONTGlyph *glyph = &(font->glyph[ch - font->first]);   							// Point to the glyph
 
-    // int height = font[2];                                                           // Total font height
-    // int width = chData[0];                                                          // Width of the glyph
-    // GFXASetPort(vp);
+	uint8_t *bitmapData = font->bitmap+glyph->bitmapOffset;  						// This points to the actual bits
+	uint8_t bitMask = 0x80;                                                         // Bit to check.
 
-    // yPos = yPos - scale * font[3];                                                  // Go to the top of the character from the baseline
-    // // TODO: Fill background ?
-    // yPos = yPos + scale * chData[1];                                                // Go to the top of the data.
+	GFXASetPort(vp);  																// Set the viewport
 
-    // if (font[4] != 0) {                                                             // Mono spaced.
-    //     xPos = xPos + font[4]/2-width/2;                                            // Centre the character in its box.
-    // }
+	xPos = xPos + scale * glyph->xOffset;
+	yPos = yPos + scale * glyph->yOffset;
+	// Background ?
 
-    // for (int y = 0;y < chData[2];y++) {                                             // For each line of actual data.
-    //     for (int x = 0;x < width;x++) {                                             // Each horizontal pixel ?
-    //         if (((*bitmapData) & bitMask) != 0) {                                   // Plot bitmap.
-    //             if (scale == 1) {                                                   // Pixel or Rectangle.
-    //                 GFXAPlot(xPos+x*scale,yPos,colour);
-    //             } else {
-    //                 GFXFillRect(vp,xPos+x*scale,yPos,xPos+x*scale+scale-1,yPos+scale-1,colour);
-    //             }
-    //         }            
-    //         bitMask >>= 1;                                                          // Next bit
-    //         if (bitMask == 0) {
-    //             bitMask = 0x80;bitmapData++;
-    //         }
-    //     }
-    //     yPos += scale;                                                              // Next line down.
-    // }
-    // offset = scale * chData[0];                                                     // Actual data offset, no spacing.
-    // return (font[4] == 0) ? offset + max(1,height/10) : font[4];
-    return 16;
+	for (int y = 0;y < glyph->height;y++) {                                         // For each line of actual data.
+	    for (int x = 0;x < glyph->width;x++) {                                      // Each horizontal pixel ?
+	        if (((*bitmapData) & bitMask) != 0) {                                   // Plot bitmap.
+	            if (scale == 1) {                                                   // Pixel or Rectangle.
+	                GFXAPlot(xPos+x*scale,yPos,colour);
+	            } else {
+	                GFXFillRect(vp,xPos+x*scale,yPos,xPos+x*scale+scale-1,yPos+scale-1,colour);
+	            }
+	        }            
+	        bitMask >>= 1;                                                          // Next bit
+	        if (bitMask == 0) {
+	            bitMask = 0x80;bitmapData++;
+	        }
+	    }
+	    yPos += scale;                                                              // Next line down.
+	}
+	return glyph->xAdvance;
 }
 
 // ***************************************************************************************
@@ -82,9 +72,9 @@ static int _DrawCharacter(GFXPort *vp,int xPos,int yPos,int ch,int fontid,int co
 // ***************************************************************************************
 
 void GFXDrawString(GFXPort *vp,int xPos,int yPos,char *s,int font,int colour,int scale) {
-    while (*s != '\0' && xPos < vp->width) {
-        int w = _DrawCharacter(vp,xPos,yPos,*s++,font,colour,scale);
-        xPos += w;
-    }
+	while (*s != '\0' && xPos < vp->width) {
+		int w = _DrawCharacter(vp,xPos,yPos,*s++,font,colour,scale);
+		xPos += w;
+	}
 }
 
--- libraries/graphics/fonts/fontindex.h
+++ libraries/graphics/fonts/fontindex.h
@@ -1,2 +1,2 @@
-static const FONTInfo *_dictionary[] = { &mono9pt7b,&mono12pt7b,&mono18pt7b,&sans9pt7b,&sans12pt7b,&sans18pt7b,&script9pt7b,&script12pt7b,&script18pt7b,&serif9pt7b,&serif12pt7b,&serif18pt7b,&symbols9pt8b,&symbols12pt8b,&symbols18pt8b };
+static const FONTInfo *_dictionary[] = { &mono7pt7b,&mono9pt7b,&mono12pt7b,&mono18pt7b,&sans7pt7b,&sans9pt7b,&sans12pt7b,&sans18pt7b,&script7pt7b,&script9pt7b,&script12pt7b,&script18pt7b,&serif7pt7b,&serif9pt7b,&serif12pt7b,&serif18pt7b,&symbols7pt8b,&symbols9pt8b,&symbols12pt8b,&symbols18pt8b };
 
