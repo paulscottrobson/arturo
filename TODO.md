0c149820685f1b15624e0b98909b723a65e458e4
Paul Robson <paul@robsons.org.uk>
2025-01-16 17:07:48 +0000

    Aquarius working

--- TODO.md
+++ TODO.md
@@ -1,7 +1,4 @@
 # TODO stuff
 
-
-
-- Spectrum wrapper.
 - Mouse code (design implement ugui)
 - Design & implement c-basic
--- applications/aquarius/aquarius.c
+++ applications/aquarius/aquarius.c
@@ -12,12 +12,13 @@
 #include "common.h"
 #include <libraries.h>
 
-uint8_t SuperRead(uint16_t a);
-void SuperWrite(uint16_t a,uint8_t d);
-uint8_t SuperPortRead(uint16_t a);
-void SuperPortWrite(uint16_t a,uint8_t d);
-uint8_t ReadKeyboard(void);
+uint8_t AquaRead(uint16_t a);
+void AquaWrite(uint16_t a,uint8_t d);
+uint8_t AquaPortRead(uint16_t a);
+void AquaPortWrite(uint16_t a,uint8_t d);
+uint8_t ReadKeyboard(int addr);
 void ScreenPaint(uint16_t addr);
+static uint8_t pickSelect(int colour,int bitMask);
 
 /**
  * @brief      Dump the current 6502 status to stdout
@@ -26,7 +27,7 @@ void ScreenPaint(uint16_t addr);
 void Dump(void) {
     static CPUZ80STATUS st;
     CPUZ80GetStatus(&st);
-    printf("%04x:%02x A:%02x B:%02x C:%02x D:%02x E:%02x H:%02x L:%02x:\n",st.pc,SuperRead(st.pc),st.a,st.b,st.c,st.d,st.e,st.h,st.l);;
+    printf("%04x:%02x A:%02x B:%02x C:%02x D:%02x E:%02x H:%02x L:%02x:\n",st.pc,AquaRead(st.pc),st.a,st.b,st.c,st.d,st.e,st.h,st.l);;
 }
 
 //
@@ -50,7 +51,7 @@ uint8_t ram[RAMSIZE+4096];                                                      
  *
  * @return     byte read from that location
  */
-uint8_t SuperRead(uint16_t a) { 
+uint8_t AquaRead(uint16_t a) { 
     if (a < 0x2000) return monitor_ROM[a];                                          // 0000-1FFF is ROM
     if (a > 0x3000 && a < RAMSIZE+0x4000) return ram[a-0x3000];                     // RAM area ?
     return 0xFF; 
@@ -62,7 +63,7 @@ uint8_t SuperRead(uint16_t a) {
  * @param[in]  a     address to write to in 64k space
  * @param[in]  d     byte to write to that location
  */
-void SuperWrite(uint16_t a,uint8_t d) {
+void AquaWrite(uint16_t a,uint8_t d) {
     if (a >= 0x3000 && a < RAMSIZE+0x4000) {                                        // RAM area ?
         bool changed = (ram[a-0x3000] != d);
         ram[a - 0x3000] = d;
@@ -79,7 +80,10 @@ void SuperWrite(uint16_t a,uint8_t d) {
  *
  * @return     Value read
  */
-uint8_t SuperPortRead(uint16_t a) {
+uint8_t AquaPortRead(uint16_t a) {
+    if ((a & 0xFF) == 0xFF) {
+        return ReadKeyboard(a);
+    }
     return 0;
 }
 
@@ -89,8 +93,7 @@ uint8_t SuperPortRead(uint16_t a) {
  * @param[in]  a     Port to write to
  * @param[in]  d     Data to write
  */
-void SuperPortWrite(uint16_t a,uint8_t d) {
-
+void AquaPortWrite(uint16_t a,uint8_t d) {
 }
 
 
@@ -108,16 +111,50 @@ bool IsKeyPressed(int c) {
     return KBDGetStateArray()[c] != 0;                                              // +ve is a keyboard key
 }
 
+static int keys[][8] = {
+    { KEY_EQUAL,KEY_BACKSPACE,KEY_HASHTILDE,KEY_ENTER,KEY_SEMICOLON,KEY_DOT,0,0 },
+    { KEY_MINUS,KEY_SLASH,KEY_0,KEY_P,KEY_L,KEY_COMMA,0,0 },
+    { KEY_9,KEY_O,KEY_K,KEY_M,KEY_N,KEY_J,0,0 },
+    { KEY_8,KEY_I,KEY_7,KEY_U,KEY_H,KEY_B,0,0 },
+    { KEY_6,KEY_Y,KEY_G,KEY_V,KEY_C,KEY_F,0,0 },
+    { KEY_5,KEY_T,KEY_4,KEY_R,KEY_D,KEY_X,0,0 },
+    { KEY_3,KEY_E,KEY_S,KEY_Z,KEY_SPACE,KEY_A,0,0 },
+    { KEY_2,KEY_W,KEY_1,-KEY_Q,KEY_MOD_LSHIFT,-KEY_MOD_LCTRL,0,0 }
+};
+
+/**
+ * @brief      Gets the bit set pattern for a specific row
+ *
+ * @param[in]  row   The row to check
+ *
+ * @return     Bit set if key is pressed in each slot.
+ */
+static int GetKeyboardRow(int row) {
+    int word = 0;
+    int p = 0;
+    while (keys[row][p] != 0) {
+        if (IsKeyPressed(keys[row][p])) word |= (1 << p);
+        p++;
+    }
+    return word;
+}
+
 /**
  * @brief      Read the keyboard depending on what value has been written to the
  *             keyboard latch, all these are active low.
  *
  * @return     Byte constructed from whichever keys are pressed, active low.
  */
-uint8_t ReadKeyboard(void) {
-    return 0;
+uint8_t ReadKeyboard(int addr) {
+    uint8_t v = 0;
+    for (int i = 0;i < 8;i++) {
+        if ((addr & (0x0100 << i)) == 0) {
+            v |= GetKeyboardRow(i);
+        }
+    }
+    v ^= 0xFF;          
+    return v;
 }
-
 
 /**
  * @brief      Update the display screen, which has had a character written to
@@ -129,23 +166,56 @@ uint8_t ReadKeyboard(void) {
 void ScreenPaint(uint16_t addr) {
     if (addr >= 0x3800) return;
     addr = addr & 0x3FF;        
+    if (addr < 0x028) return;                                                       // Off the top
     int c = ram[addr];                                                              // Character
-    int x = addr % 40,y = addr / 40;                                                // Cell position.
+    int colour = ram[addr+0x400];
+    int x = addr % 40,y = addr / 40-1;                                              // Cell position.
     if (y >= 24) return;                                                            // Off the display
     int offset = x + y * 320;                                                       // Offset into the bitmap
 
     const uint8_t *fontData = character_ROM + c * 8;                                // Character data to copy
     struct DVIModeInformation *dmi = DVIGetModeInformation();                       // Access bitmap planes
 
+    static uint8_t patterns[] = { 0,0,0,0xFF };
+    static uint8_t select[3];
+
+    select[0] = pickSelect(colour,0x01);
+    select[1] = pickSelect(colour,0x02);
+    select[2] = pickSelect(colour,0x04);
+
     for (int i = 0;i < 8;i++) {                                                     // Write to bitmap
-        uint8_t bd = *fontData++;                                                   // Get the character line.
-        *(dmi->bitPlane[0]+offset) = bd;                                            // Write to RGB bitmaps
-        *(dmi->bitPlane[1]+offset) = bd;
-        *(dmi->bitPlane[2]+offset) = bd;
+        patterns[1] = *fontData++;                                                  // Get the character line.
+        patterns[2] = patterns[1] ^ 0xFF;
+        *(dmi->bitPlane[0]+offset) = patterns[select[0]];                           // Write to RGB bitmaps
+        *(dmi->bitPlane[1]+offset) = patterns[select[1]];
+        *(dmi->bitPlane[2]+offset) = patterns[select[2]];
         offset += 320/8;                                                            // One line down
     }
 }
 
+static uint8_t FourToThree[16] = {                                                  // Map four bit colour to 3 bit colour.
+    0,1,2,3,4,5,6,7,
+    7,6,5,4,3,2,1,0
+};
+
+/**
+ * @brief      For colour bit bitMask, workout the data to be put in this plane
+ *
+ * @param[in]  colour   The colour in ffff/bbbb format.
+ * @param[in]  bitMask  The bit mask in the colour.
+ *
+ * @return     0 (clear) 1 (byte) 2 (~byte) 3 (all set)
+ */
+
+static uint8_t pickSelect(int colour,int bitMask) {
+    int fgr = FourToThree[colour >> 4];
+    int bgr = FourToThree[colour & 0x0F];
+    if ((fgr & bitMask) == (bgr & bitMask)) {
+        return (fgr & bitMask) ? 3 : 0;
+    } else {
+        return (fgr & bitMask) ? 1 : 2;
+    }
+}
 /**
  * @brief      Run the main application
  */
@@ -153,8 +223,8 @@ void ApplicationRun(void) {
     CONEnableConsole(true);                                                         // Disable console output.
     GFXSetMode(DVI_MODE_320_240_8);                                                 // Display to 320x240x8 mode.
     CPUZ80SETUP s;                                                                  // Set up the processor r/w
-    s.read = SuperRead;s.write = SuperWrite;
-    s.readPort = SuperPortRead;s.writePort = SuperPortWrite;
+    s.read = AquaRead;s.write = AquaWrite;
+    s.readPort = AquaPortRead;s.writePort = AquaPortWrite;
     s.clockSpeed = 3580000;                                                         // 3.58Mhz Clock
     s.frameRate = 50;                                                               // 50 Frames/Second 
     CPUZ80Setup(&s);                                                                // Set up the processor
